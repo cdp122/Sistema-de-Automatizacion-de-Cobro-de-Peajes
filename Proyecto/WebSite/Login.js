7e245ceb539a54135d57cf0bfd077a31057f4bbe
Adrian Urresta <152809078+Mclxv1nx@users.noreply.github.com>
2024-05-25 20:41:53 -0500

    Cambios de css y añadir en js
    Noche y Dia para el login

--- Proyecto/WebSite/Login.js
+++ Proyecto/WebSite/Login.js
@@ -1,5 +1,23 @@
 
-        function validateInput(input) {
-            input.value = input.value.replace(/[^0-9]/g, '').slice(0, 10);
-        }
+function validateInput(input) {
+    input.value = input.value.replace(/[^0-9]/g, '').slice(0, 10);
+}
+
+function setBackgroundBasedOnTime() {
+    var hour = new Date().getHours();
+    var body = document.body;
+    var fullSection = document.querySelector('.full');
+    var contentSection = document.querySelector('.content');
+    var butonSection = document.querySelector('.btn');
+
+    if (hour >= 6 && hour < 18) {
+        fullSection.className = 'full day';
+    } else {
+        fullSection.className = 'full night';
+        contentSection.className = 'content contentNight';
+        butonSection.className = 'btn btnNight';
+    }
+}
+
+setBackgroundBasedOnTime();
     
