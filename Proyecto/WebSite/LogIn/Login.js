ccf7ba05ae3dee6d8cd8ad340ae7a6d71c0c0d09
Adrian Urresta <152809078+Mclxv1nx@users.noreply.github.com>
2024-06-09 08:31:04 -0500

    Orden en Carpetas
    Orden en carpetas y con los cambios respectivos de las direcciones de memoria para funcnionamiento de las rutas de las paginas web, no de la base de datos

---
+++ Proyecto/WebSite/LogIn/Login.js
@@ -0,0 +1,45 @@
+function validateInput(input) {
+    input.value = input.value.replace(/[^0-9]/g, '').slice(0, 11);
+}
+
+function setBackgroundBasedOnTime() {
+    var hour = new Date().getHours();
+    var fullSection = document.querySelector('.full');
+    var contentSection = document.querySelector('.content');
+    var buttonSection = document.querySelector('.btn');
+    var usernameInput = document.querySelector('.username');
+    var passwordInput = document.querySelector('.contraseña');
+
+    if (hour >= 6 && hour < 18) {
+        fullSection.className = 'full day';
+    } else {
+        fullSection.className = 'full night';
+        contentSection.className = 'content contentNight';
+        buttonSection.className = 'btn btnNight';
+        usernameInput.className = 'username usernameNight';
+        passwordInput.className = 'contraseña contraseñaNight';
+    }
+}
+
+setBackgroundBasedOnTime();
+
+
+async function Validar() {
+    const user = document.getElementById("username").value;
+    const contraseña = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("/login/auth?username="
+            + encodeURIComponent(user) + "&password=" +
+            encodeURIComponent(contraseña), { method: 'GET' });
+        const result = await response.json();
+        if (response.ok) {
+            localStorage.setItem('token', result.token);
+            window.location.href = "./Profile.html"; //aqui va la dirección
+        } else {
+            alert(result.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
