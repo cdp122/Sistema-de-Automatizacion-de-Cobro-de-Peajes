707f02686b97c60ff4d0ed6d397ba02a4ec2d671
Sebastian-1232 <mateosebasrocha7@gmail.com>
2024-06-18 08:39:40 -0500

    Cambios en la pagina de Telepass y Contacto

---
+++ Proyecto/WebSite/Home/Script/telepass.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const despDiv = document.querySelector(".Desp");
+    const navLinks = document.querySelectorAll(".navDes a");
+
+    menuToggle.addEventListener("change", () => {
+        if (menuToggle.checked) {
+            despDiv.style.display = "block";
+        } else {
+            despDiv.style.display = "none";
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            menuToggle.checked = false;
+            despDiv.style.display = "none";
+        });
+    });
+});
